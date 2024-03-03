9684178bf8d8951e28f378b236e26784d92371d6
brett.baek <brett.baek@ihsmarkit.com>
2024-03-02 21:29:14 -0700

    In the midst of initializing the project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import PetFound from './components/PetFound.vue'
+import PetFoundLogo from './assets/pet-found-logo.jpg'
+</script>
+
+<template>
+  <div class="app-container">
+    <div class="pet-found-logo">
+			<img alt="Pet Found Logo" class="logo" :src="PetFoundLogo" width="125" height="125" />
+		</div>
+    <div class="navigation">
+      <PetFound msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/PetFound.vue
@@ -0,0 +1,22 @@
+<script setup>
+import PetFoundLogo from '../assets/pet-found-logo.jpg'
+
+</script>
+
+<template>
+  <div class="pet-found-logo">
+		<img alt="Pet Found Logo" class="logo" :src="PetFoundLogo" width="125" height="125" />
+	</div>
+	<div class="navigation">
+		<PetFound msg="You did it!" />
+
+		<nav>
+			<RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/about">About</RouterLink>
+		</nav>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
